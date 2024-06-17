b74460f1efa347fbfbaba0ffe60f72d1c9aa5313
ravi9559 <ravi.eng2012@gmail.com>
2024-06-17 12:29:34 +0530

    react components

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,14 +1,47 @@
 
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth'
+import { logout,login } from './store/authSlice'
+import { Footer, Header } from './components'
 
 function App() {
- 
 
-  return (
-    <>
-      <h1>A Blog App with Appwrite</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((useData)=>{
+      if(useData){
+        dispatch(login({useData}))
+      } else{
+        dispatch(logout)
+      }
+    })
+    .finally( ()=>setLoading(false))
+
+
+  },[])
+ 
+   return !loading ? (
+
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400 '>
+      <div className='w-full block'>
+        <Header />
+        <main>
+         TODO: {/* {<Outlet />} */}
+        </main>
+        <Footer />
+      </div>
+      
+     </div>
+
+   ) : null
+
+  
 }
 
 export default App
---
+++ 12MegaBlog/src/components/Input.jsx
@@ -0,0 +1,19 @@
+import React, { useId } from "react";
+
+
+const Input =React.forwardRef(function Input({
+
+    label,
+    type ='text',
+    className ="",
+    ...props
+
+
+
+}),ref{
+})
+
+
+
+
+export default Input
